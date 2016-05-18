5a57a645889868416ff24f59bd85d720580c1f88
Dan Aukes <danaukes@asu.edu>
2016-05-18 02:53:05 -0700

    added travis script and test1.py

--- genericshapes.py
+++ genericshapes.py
@@ -36,18 +36,4 @@ class GenericPoly(genericshapebase.GenericShapeBase,shapely_algebra.ClassAlgebra
 
         
 if __name__=='__main__':
-    exterior = [[0,0],[0,1],[1,2],[2,1],[2,-1],[1,-2],[0,-1]]
-    exterior = [tuple(item) for item in exterior]
-    exterior2 = [(.5,0),(1,3**.5/2),(1.5,0)]
-    a = GenericPoly(exterior,[])
-    b = GenericPoly(exterior2,[])
-    a.plot()
-    b.plot()
-#    A=a.to_shapely()
-#    B=b.to_shapely()
-#    C = A.difference(B)
-#    c=csg_shapely.to_generic(C)
-    c = a-b
-    
-    plt.figure()
-    c.plot()
+  pass
---
+++ test1.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+from genericshapes import GenericPoly
+
+if __name__=='__main__':
+    exterior = [[0,0],[0,1],[1,2],[2,1],[2,-1],[1,-2],[0,-1]]
+    exterior = [tuple(item) for item in exterior]
+    exterior2 = [(.5,0),(1,3**.5/2),(1.5,0)]
+    a = GenericPoly(exterior,[])
+    b = GenericPoly(exterior2,[])
+#    a.plot()
+#    b.plot()
+#    A=a.to_shapely()
+#    B=b.to_shapely()
+#    C = A.difference(B)
+#    c=csg_shapely.to_generic(C)
+    c = a-b
+    
+#    plt.figure()
+#    c.plot()
