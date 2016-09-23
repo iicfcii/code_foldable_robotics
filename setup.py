32b58c4db35b3897e9343ad60dec6072d3eaa965
Dan Aukes <danaukes@gmail.com>
2016-09-23 10:16:22 -0700

    updated version and published.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ package_data['foldable_robotics'] = []
 
 setup_kwargs = {}
 setup_kwargs['name']='foldable_robotics'
-setup_kwargs['version']='0.0.1'
+setup_kwargs['version']='0.0.2'
 setup_kwargs['classifiers']=['Programming Language :: Python','Programming Language :: Python :: 3']   
 setup_kwargs['description']='Foldable robotics is a package for designing and analyzing foldable laminate robots'
 setup_kwargs['author']='Dan Aukes'
