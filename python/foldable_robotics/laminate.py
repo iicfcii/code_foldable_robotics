9ecb55679194d2566d2c4a28cbd50db32a4e4104
Dan Aukes <danaukes@gmail.com>
2017-12-21 14:53:16 -0700

    added documentation

--- python/foldable_robotics/laminate.py
+++ python/foldable_robotics/laminate.py
@@ -14,20 +14,42 @@ import numpy
 from idealab_tools.iterable import Iterable
 
 class WrongNumLayers(Exception):
+    '''Custom exception for when two laminates of the wrong number of layers interact.'''
     pass
 
 class Laminate(Iterable,ClassAlgebra):
+    '''The Laminate class holds a list of layers and can be operated upon with CSG-style functions.'''
     def __init__(self, *layers):
+        '''
+        Initializes the class
+        
+        :param layers: A list of layers which compose the laminate
+        :type layers: list
+        :rtype: Laminate
+        '''
         self.layers = list(layers)
         self.id = id(self)
 
     def copy(self,identical = True):
+        '''
+        creates a copy of the instance
+        
+        :param identical: whether to use the same id or not.
+        :type identical: boolean
+        :rtype: Laminate
+        '''        
         new = type(self)(*[layer.copy(identical) for layer in self.layers])
         if identical:        
             new.id = self.id
         return new
 
     def export_dict(self):
+        '''
+        converts the laminate to a dict.
+        
+        :rtype: dict
+        '''        
+
         d = {}
         d['layers'] = [layer.export_dict() for layer in self.layers]
         d['id'] = self.id
@@ -35,11 +57,26 @@ class Laminate(Iterable,ClassAlgebra):
 
     @classmethod
     def import_dict(cls,d):
+        '''
+        converts a dict to a Laminate class
+        
+        :param d: the laminate in dict form
+        :type d: dict
+        :rtype: Laminate
+        '''        
+        
         new = cls(*[Layer.import_dict(item) for item in d['layers']])
         new.id = d['id']
         return new
 
     def plot(self,new=False):
+        '''
+        plots the laminate using matplotlib.
+        
+        :param new: whether to create a new figure
+        :type new: boolean
+        '''   
+        
         import matplotlib.cm
         cm = matplotlib.cm.plasma
         l = len(self.layers)     
@@ -52,6 +89,10 @@ class Laminate(Iterable,ClassAlgebra):
             layer.plot(color = color)
 
     def plot_layers(self):
+        '''
+        plots each layer of the laminate in a new figure using matplotlib.
+        '''   
+                
         import matplotlib.cm
         cm = matplotlib.cm.plasma
         l = len(self.layers)        
@@ -61,19 +102,45 @@ class Laminate(Iterable,ClassAlgebra):
     
     @property
     def list(self):
+        '''converts the laminate to a list.'''
         return self.layers
 
     def binary_operation(self,function_name,other,*args,**kwargs):
+        '''
+        performs a binary operation between self and other.
+        
+        :param function_name: the layer-based function to be performed
+        :type function_name: string
+        :param other: the layer-based function to be performed
+        :type other: Laminate
+        :param *args: tuple of arguments passed to subfunction
+        :type *args: tuple
+        :param *kwargs: keyword arguments passed to subfunction
+        :type *kwargs: dict
+        :rtype: Laminate
+        '''
+
         if len(self.layers)!=len(other.layers):
             raise(WrongNumLayers())
         else:
             layers = []
             for layer1,layer2 in zip(self.layers,other.layers):
                 function = getattr(layer1,function_name)
-                layers.append(function(layer2))
+                layers.append(function(layer2,*args,**kwargs))
             return type(self)(*layers)
 
     def unary_operation(self,function_name,*args,**kwargs):
+        '''
+        performs a unary operation on the laminate.
+        
+        :param function_name: the layer-based function to be performed
+        :type function_name: string
+        :param *args: tuple of arguments passed to subfunction
+        :type *args: tuple
+        :param *kwargs: keyword arguments passed to subfunction
+        :type *kwargs: dict
+        :rtype: Laminate
+        '''
             layers = []
             for layer1 in self.layers:
                 function = getattr(layer1,function_name)
@@ -81,51 +148,184 @@ class Laminate(Iterable,ClassAlgebra):
             return type(self)(*layers)
             
     def union(self,other):
+        '''
+        unions two laminates together.
+        
+        :param other: the layer-based function to be performed
+        :type other: Laminate
+        :rtype: Laminate
+        '''
         return self.binary_operation('union',other)
 
     def difference(self,other):
+        '''
+        takes the difference of self - other.
+        
+        :param other: the layer-based function to be performed
+        :type other: Laminate
+        :rtype: Laminate
+        '''
         return self.binary_operation('difference',other)
 
     def symmetric_difference(self,other):
+        '''
+        takes the symmetric difference of self and other.
+        
+        :param other: the layer-based function to be performed
+        :type other: Laminate
+        :rtype: Laminate
+        '''
         return self.binary_operation('symmetric_difference',other)
 
     def intersection(self,other):
+        '''
+        takes the intersection of self and other.
+        
+        :param other: the layer-based function to be performed
+        :type other: Laminate
+        :rtype: Laminate
+        '''
         return self.binary_operation('intersection',other)
     
     def buffer(self,*args,**kwargs):
+        '''
+        dilate or erode the laminate based on the arguments sent.
+        see the corresponding layer function for arguments and keyword-arguments
+        
+        :param *args: tuple of arguments passed to subfunction
+        :type *args: tuple
+        :param *kwargs: keyword arguments passed to subfunction
+        :type *kwargs: dict
+        :rtype: Laminate
+        '''
         return self.unary_operation('buffer',*args,**kwargs)
 
     def dilate(self,*args,**kwargs):
+        '''
+        dilate the laminate.
+        see the corresponding layer function for arguments and keyword-arguments
+        
+        :param *args: tuple of arguments passed to subfunction
+        :type *args: tuple
+        :param *kwargs: keyword arguments passed to subfunction
+        :type *kwargs: dict
+        :rtype: Laminate
+        '''
         return self.unary_operation('dilate',*args,**kwargs)
 
     def erode(self,*args,**kwargs):
+        '''
+        erode the laminate.
+        see the corresponding layer function for arguments and keyword-arguments
+        
+        :param *args: tuple of arguments passed to subfunction
+        :type *args: tuple
+        :param *kwargs: keyword arguments passed to subfunction
+        :type *kwargs: dict
+        :rtype: Laminate
+        '''
         return self.unary_operation('erode',*args,**kwargs)
 
     def translate(self,*args,**kwargs):
+        '''
+        translate the laminate.
+        see the corresponding layer function for arguments and keyword-arguments
+        
+        :param *args: tuple of arguments passed to subfunction
+        :type *args: tuple
+        :param *kwargs: keyword arguments passed to subfunction
+        :type *kwargs: dict
+        :rtype: Laminate
+        '''
         return self.unary_operation('translate',*args,**kwargs)
         
     def rotate(self,*args,**kwargs):
+        '''
+        rotate the laminate.
+        see the corresponding layer function for arguments and keyword-arguments
+        
+        :param *args: tuple of arguments passed to subfunction
+        :type *args: tuple
+        :param *kwargs: keyword arguments passed to subfunction
+        :type *kwargs: dict
+        :rtype: Laminate
+        '''
         return self.unary_operation('rotate',*args,**kwargs)
 
     def scale(self,*args,**kwargs):
+        '''
+        scale the laminate.
+        see the corresponding layer function for arguments and keyword-arguments
+        
+        :param *args: tuple of arguments passed to subfunction
+        :type *args: tuple
+        :param *kwargs: keyword arguments passed to subfunction
+        :type *kwargs: dict
+        :rtype: Laminate
+        '''
         return self.unary_operation('scale',*args,**kwargs)
 
     def affine_transform(self,*args,**kwargs):
+        '''
+        affine transform the laminate.
+        see the corresponding layer function for arguments and keyword-arguments
+        
+        :param *args: tuple of arguments passed to subfunction
+        :type *args: tuple
+        :param *kwargs: keyword arguments passed to subfunction
+        :type *kwargs: dict
+        :rtype: Laminate
+        '''
         return self.unary_operation('affine_transform',*args,**kwargs)
 
     def simplify(self,*args,**kwargs):
+        '''
+        simplify the laminate.
+        see the corresponding layer function for arguments and keyword-arguments
+        
+        :param *args: tuple of arguments passed to subfunction
+        :type *args: tuple
+        :param *kwargs: keyword arguments passed to subfunction
+        :type *kwargs: dict
+        :rtype: Laminate
+        '''
         return self.unary_operation('simplify',*args,**kwargs)
 
     def map_line_stretch(self,*args,**kwargs):
+        '''
+        transform the laminate based on two lines.
+        see foldable_robotics.manufacturing.map_line_stretch for arguments and keyword-arguments
+        
+        :param *args: tuple of arguments passed to subfunction
+        :type *args: tuple
+        :param *kwargs: keyword arguments passed to subfunction
+        :type *kwargs: dict
+        :rtype: Laminate
+        '''
         import foldable_robotics.manufacturing
         return foldable_robotics.manufacturing.map_line_stretch(self,*args,**kwargs)
         
     def export_dxf(self,name):
+        '''
+        export the laminate to a .dxf file
+        see the corresponding layer function for arguments and keyword-arguments
+        
+        :param name: filename to export
+        :type name: string
+        '''
         for ii,layer in enumerate(self.layers):
             layername = name+str(ii)
             layer.export_dxf(layername)
             
     def mesh_items(self,material_properties):
+        '''
+        create a mesh for 3d rendering using pyqtgraph
+        
+        :param material_properties: tuple of arguments passed to subfunction
+        :type material_properties: foldable_robotics.dynamics_info.MaterialProperty
+        :rtype: GLMeshItem
+        '''
+        
         import matplotlib.cm as cm
         import pyqtgraph.opengl as gl
 
@@ -147,6 +347,13 @@ class Laminate(Iterable,ClassAlgebra):
         return mi
 
     def mass_properties(laminate,material_properties):
+        '''
+        calculate mass properties
+        
+        :param material_properties: tuple of arguments passed to subfunction
+        :type material_properties: foldable_robotics.dynamics_info.MaterialProperty
+        :rtype: tuple of mass properties
+        '''        
         volume = 0
         mass = 0
         z=0
