e402ab550bab078f6344a88b98ba4251556a401c
Dan Aukes <danaukes@gmail.com>
2019-02-04 10:27:20 -0700

    fixing interior exterior stuff

--- python/foldable_robotics_tests/make_pdf.py
+++ python/foldable_robotics_tests/make_pdf.py
@@ -10,6 +10,6 @@ import foldable_robotics.pdf
 d = foldable_robotics_tests.manufacturing_example1.worst_case_supported_design.translate(10,10)[-1]
 d=d.scale(foldable_robotics.pdf.ppi,foldable_robotics.pdf.ppi)
 p=foldable_robotics.pdf.Page('test.pdf')
-for item in d.exteriors()+d.interiors():
+for item in d.get_paths():
     p.draw_poly(item)
 p.close()
